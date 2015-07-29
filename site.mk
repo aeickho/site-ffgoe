0a6f4163a86ba5afad9a29448a52d11958a96407
alexander eickhoff <alex@artbanause.de>
2015-07-29 13:56:55 +0200

    - gluon-legacy

--- site.mk
+++ site.mk
@@ -11,7 +11,6 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
@@ -25,12 +24,11 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 2015.1.x_exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7.exp-$(shell date '+%Y%m%d%H%M')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
 
-# Languages to include
 GLUON_LANGS ?= de en
